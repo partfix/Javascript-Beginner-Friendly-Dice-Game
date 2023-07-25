47f610758865566e6004eeb732f54a3a4c3b2c0f
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-07-26 02:14:19 +0800

    add features and functionality

---
+++ jsFolder/index.js
@@ -0,0 +1,25 @@
+function randomNumber() {
+	return Math.floor(Math.random() * 6 + 1); // this is random number from 1-6
+}
+
+function gameFunction() {
+	let holder = randomNumber();
+	let randomImages = "pictures/dice" + holder + ".png";
+	document.querySelectorAll("img")[0].setAttribute("src", randomImages);
+
+	let holder1 = randomNumber();
+	let randomImages1 = "pictures/dice" + holder1 + ".png";
+	document.querySelectorAll("img")[1].setAttribute("src", randomImages1);
+
+	if (randomImages > randomImages1) {
+		document.querySelector("h1").innerHTML = "Player 1 Wins!!!";
+	} else if (randomImages < randomImages1) {
+		document.querySelector("h1").innerHTML = "Player 2 Wins!!!";
+	} else {
+		document.querySelector("h1").innerHTML = "Draw!!!";
+		document.querySelector("body").style.backgroundImage =
+			"url('pictures/confetti.gif')";
+	}
+}
+
+gameFunction();
